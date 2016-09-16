bc19d70a63f7260154023c59148473a299366f4f
usernamesuggestion <usernamesuggestion@gmail.com>
2016-09-15 20:44:12 -0600

    updated class/csproj

--- Player/Player/PlayerClass.cs
+++ Player/Player/PlayerClass.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Player
 {
-    public class Class1
+    public class PlayerClass
     {
         //Secret Message
         private int strength { get; set; }
